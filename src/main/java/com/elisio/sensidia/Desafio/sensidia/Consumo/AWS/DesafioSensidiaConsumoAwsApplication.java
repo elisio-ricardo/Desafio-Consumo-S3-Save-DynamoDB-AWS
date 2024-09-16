aa3498f417ab62ed84e5e9ab8d709ef1307fcb6b
Elisio Ricardo <elisio.silva@sysmap.com>
2024-09-16 09:06:55 -0300

    adicionando base initializer

---
+++ src/main/java/com/elisio/sensidia/Desafio/sensidia/Consumo/AWS/DesafioSensidiaConsumoAwsApplication.java
@@ -0,0 +1,13 @@
+package com.elisio.sensidia.Desafio.sensidia.Consumo.AWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSensidiaConsumoAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSensidiaConsumoAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elisio/sensidia/Desafio/sensidia/Consumo/AWS/DesafioSensidiaConsumoAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elisio.sensidia.Desafio.sensidia.Consumo.AWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSensidiaConsumoAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
